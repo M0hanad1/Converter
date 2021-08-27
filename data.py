b08a2b8daa57d9cca66d65ea38cfc1f4b5311a83
Mohanad <mohned.dabsha@gmail.com>
2021-08-27 23:04:11 +0200

    Add from hex to binary function

--- data.py
+++ data.py
@@ -163,7 +163,17 @@ class Hexadecimal:
         return result
 
     def to_binary(self) -> str:
-        return
+        num = self.num
+        _hex = {
+            "0": "0000", "1": "0001", "2": "0010", "3": '0011', "4": "0100", "5": "0101", "6": "0110", "7": "0111",
+            "8": "1000", "9": "1001", "A": "1010", "B": "1011", "C": "1100", "D": "1101", "E": "1110", "F": "1111"
+        }
+        result = ""
+
+        for i in num:
+            result += _hex[i]
+
+        return result
 
     def to_octal(self) -> int:
         return
@@ -183,7 +193,7 @@ class Octal:
         return
 
 
-# num = Hexadecimal("7DE")
+# num = Hexadecimal("DE")
 # print(num.to_binary())
 # print(num.to_decimal())
 # print(num.to_octal())
