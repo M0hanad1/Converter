88fb66c7733526ea753cd9a23ff4b676e5103794
Mohanad <mohned.dabsha@gmail.com>
2021-08-27 02:22:09 +0200

    Write the classes of data

---
+++ data.py
@@ -0,0 +1,54 @@
+class Decimal:
+    def __init__(self, num: int) -> None:
+        self.num = num
+
+    def to_binary(self) -> str:
+        return
+
+    def to_hex(self) -> str:
+        return
+
+    def to_octal(self) -> int:
+        return
+
+
+class Bianry:
+    def __init__(self, num: str) -> None:
+        self.num = num
+
+    def to_deimal(self) -> int:
+        return
+
+    def to_hex(self) -> str:
+        return
+
+    def to_octal(self) -> int:
+        return
+
+
+class Hexadecimal:
+    def __init__(self, num: str) -> None:
+        self.num = num
+
+    def to_deimal(self) -> int:
+        return
+
+    def to_binary(self) -> str:
+        return
+
+    def to_octal(self) -> int:
+        return
+
+
+class Octal:
+    def __init__(self, num: int) -> None:
+        self.num = num
+
+    def to_deimal(self) -> int:
+        return
+
+    def to_binary(self) -> str:
+        return
+
+    def to_hex(self) -> str:
+        return
